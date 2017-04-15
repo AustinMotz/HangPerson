7c115f3bc2076a5ae32583cf542ed1b72c82c1f7
Austin <austinmotz2@gmail.com>
2017-04-15 18:02:57 -0400

    removed unused imports

--- HangPerson/src/HangPerson/HangPerson.java
+++ HangPerson/src/HangPerson/HangPerson.java
@@ -2,14 +2,10 @@ package HangPerson;
 
 import java.util.*;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.image.BufferedImage;
 import java.io.*;
 
 
